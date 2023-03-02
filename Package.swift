5289d84be4159bf69f87069ecd19d00537d98028
hiletmis <hayrettin@ennowallet.com>
2023-03-02 14:12:13 +0300

    initial commit

--- Package.swift
+++ Package.swift
@@ -7,22 +7,22 @@ let package = Package(
     name: "WavesUtil",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "WavesUtil",
-            targets: ["WavesUtil"]),
+        .library( name: "WavesUtil", targets: ["WavesUtil"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "WavesUtil",
-            dependencies: []),
-        .testTarget(
-            name: "WavesUtilTests",
-            dependencies: ["WavesUtil"]),
+        .target( name: "WavesUtil", dependencies: ["Crpytoworks", "Keccak", "Curve25519", "Blake2", "Ed25519"]),
+        .target( name: "Crpytoworks", dependencies: []),
+        .target( name: "Keccak", dependencies: []),
+        .target( name: "Ed25519", dependencies: ["Curve25519"]),
+        .target( name: "Curve25519", dependencies: []),
+        .target( name: "Blake2", dependencies: []),
+        .testTarget( name: "WavesUtilTests", dependencies: ["WavesUtil"]),
     ]
 )
--- Tests/WavesUtilTests/WavesUtilTests.swift
+++ Tests/WavesUtilTests/WavesUtilTests.swift
@@ -6,6 +6,10 @@ final class WavesUtilTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WavesUtil().text, "Hello, World!")
+        XCTAssertEqual(Base58Encoder.decode("39wFF1"), [84, 69, 83, 84])
+        XCTAssertEqual(Base58Encoder.decodeToStr("39wFF1"), "TEST")
+        XCTAssertEqual(Base58Encoder.encode([84, 69, 83, 84]), "39wFF1")
+        XCTAssertEqual(Base58Encoder.validate("39wFF1"), true)
+        
     }
 }
