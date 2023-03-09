173c7f2b33def1e94c0ed7092b7cbce90e00024e
hiletmis <hayrettin@ennowallet.com>
2023-03-09 09:47:26 +0300

    secp256k1 included

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "WavesUtil",
+    name: "EnnoUtil",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "WavesUtil", targets: ["WavesUtil"]),
+        .library( name: "EnnoUtil", targets: ["EnnoUtil"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target( name: "WavesUtil", dependencies: ["Crpytoworks", "Keccak", "Curve25519", "Blake2", "Ed25519"]),
+        .target( name: "EnnoUtil", dependencies: ["Crpytoworks", "Keccak", "Curve25519", "Blake2", "Ed25519", "secp256k1"]),
         .target( name: "Crpytoworks", dependencies: []),
         .target( name: "Keccak", dependencies: []),
         .target( name: "Ed25519", dependencies: ["Curve25519"]),
         .target( name: "Curve25519", dependencies: []),
+        .target( name: "secp256k1", dependencies: []),
         .target( name: "Blake2", dependencies: []),
-        .testTarget( name: "WavesUtilTests", dependencies: ["WavesUtil"]),
+        .testTarget( name: "EnnoUtilTests", dependencies: ["EnnoUtil"]),
     ]
 )
--- Tests/EnnoUtilTests/EnnoUtilTests.swift
+++ Tests/EnnoUtilTests/EnnoUtilTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import WavesUtil
+@testable import EnnoUtil
 
 final class WavesUtilTests: XCTestCase {
     
